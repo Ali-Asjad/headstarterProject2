8c61e32637909f1569267e8f74f83b05c23a42fb
Ali-Asjad <aliasjadwork@gmail.com>
2024-08-08 01:17:55 -0400

    added the addItem button

--- app/page.js
+++ app/page.js
@@ -141,9 +141,14 @@ export default function Home() {
               >
                 <Typography variant="h3" color="#333" textAlign="center">{name.charAt(0).toUpperCase() + name.slice(1)}</Typography>
                 <Typography variant="h3" color="#333" textAlign="center">{quantity}</Typography>
-                <Button variant="contained" onClick={()=>{
-                  removeItem(name)
-                }}>Remove</Button>
+                <Stack direction="row" spacing={2}>
+                  <Button variant="contained" onClick={()=>{
+                    addItem(name)
+                  }}>Add</Button>
+                  <Button variant="contained" onClick={()=>{
+                    removeItem(name)
+                  }}>Remove</Button>
+                </Stack>
               </Box>
             ))
           }
